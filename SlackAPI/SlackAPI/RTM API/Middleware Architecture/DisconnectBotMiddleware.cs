1294232fc551212c705fe8244bccba17f4c0b57d
Orhan Asan <orhanasan97@gmail.com>
2018-07-07 22:36:55 +0300

    Implemented Pipeline-ish structure for RTM API
    Pipeline architecture is implemented and by default it has 3 middleware; namely help, stats and disconnect bot commands. From now on, coder can implement any middleware by inheriting IMiddleware interface and add it to the pipeline. Coder should pass the needed objects (WebSocket, SlackClient, ...) by using a Dictionary<string, object> and parse them in his/her middleware's Process method (Process method strictly takes Dictionary<string, object> as argument). One can look for examples (i.e HelpMiddleware, StatsMiddleware ...) in the source code.

---
+++ SlackAPI/SlackAPI/RTM API/Middleware Architecture/DisconnectBotMiddleware.cs
@@ -0,0 +1,63 @@
+﻿using Newtonsoft.Json;
+using SlackAPI.Conversations;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using WebSocketSharp;
+
+namespace SlackAPI.RTM_API.Middleware_Architecture
+{
+    public class DisconnectBotMiddleware : IMiddleware
+    {
+        public bool IsComplete { get; private set; }
+
+        public string Description { get; private set; }
+
+        public string Command { get; private set; }
+
+        public void Process(Dictionary<string, object> parameters)
+        {
+            IsComplete = false;
+            Message message = null;
+            List<string> botParameters = null;
+            Stats stats = null;
+            SlackClient slackClient = null;
+            string userName = null;
+            string botName = null;
+            WebSocket webSocket = null;
+            foreach (var item in parameters)
+            {
+                switch (item.Key)
+                {
+                    case "stats": stats = (Stats)item.Value; break;
+                    case "userName": userName = (string)item.Value; break;
+                    case "message": message = (Message)item.Value; break;
+                    case "parameters": botParameters = (List<string>)item.Value; break;
+                    case "_botName": botName = (string)item.Value; break;
+                    case "_slackClient": slackClient = (SlackClient)item.Value; break;
+                    case "_ws": webSocket = (WebSocket)item.Value; break;
+                    default:
+                        break;
+                }
+            }
+            Models.Attachment attachment = new Models.Attachment
+            {
+                Footer = "BordaBot",
+                Ts = Extension.ToProperTimeStamp(DateTime.Now)
+            };
+            if (botParameters[1] == "disconnect" && botParameters[2] == "bot")
+            {
+                stats.MessageReceived();
+                attachment.Color = "danger";
+                attachment.Text = botName + " is shutting down.";
+                string attachments = "[" + JsonConvert.SerializeObject(attachment) + "]";
+
+                slackClient.PostMessage(message.Channel, "@" + userName + ":", false, attachments);
+                webSocket.Close();
+                IsComplete = true;
+            }
+            Command = "disconnect bot";
+            Description = "To make " + botName + " disconnect from this wokspace";
+        }
+    }
+}
---
+++ SlackAPI/SlackAPI/RTM API/Middleware Architecture/IMiddleware.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using WebSocketSharp;
+
+namespace SlackAPI.RTM_API.Middleware_Architecture
+{
+    public interface IMiddleware
+    {
+        void Process(Dictionary<string, object> parameters);
+
+        bool IsComplete { get; }
+
+        string Description { get; }
+
+        string Command { get; }
+    }
+}
